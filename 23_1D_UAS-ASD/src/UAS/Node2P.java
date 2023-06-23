2cfb1711c934104571e306aa94e048067de2c061
radenariq <ariqresya42@gmail.com>
2023-06-23 09:35:43 +0700

    UAS

---
+++ 23_1D_UAS-ASD/src/UAS/Node2P.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UAS;
+
+/**
+ *
+ * @author Asus
+ */
+public class Node2P {
+    int data;
+    Node2P next;
+    Node2P prev;
+
+    public Node2P(int data) {
+        this.data = data;
+        next = prev = null;
+    }
+}
